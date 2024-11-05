816a5231f3549e44dd00497a5bdc69501a0d1104
nizn1770 <124064286+nizn1770@users.noreply.github.com>
2024-11-05 11:15:06 -0600

    variable consistency and begin work on submit

--- touchscreen.py
+++ touchscreen.py
@@ -16,37 +16,43 @@ class Application(tk.TK):
 
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
-        
-        len = InputMeasure(self, "Length")
-        len.grid(row=0, column=0, sticky="nsew")
 
-        wid = InputMeasure(self, "Width")
-        wid.grid(row=0, column=0, sticky="nsew")
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=2)
+        
+        hor = InputMeasure(self, "Horizontal")
+        hor.grid(row=0, column=0, sticky="nsew")
+
+        vert = InputMeasure(self, "Vertical")
+        vert.grid(row=1, column=0, sticky="nsew")
+
+        submit = ttk.Button(self, text="Send Cut", command=self.send_cut)
+    
+
 
 class InputMeasure(ttk.frame):
-    def __init__(self, parent):
+    def __init__(self, parent, title_text):
         super().__init__(parent)
 
-        self.title = ttk.Label(self)
-
+        self.title = ttk.Label(self, text=title_text, justify="center")
+        self.title.grid(row=0, column=0, columnspan=3, sticky="nsew")
 
         self.feet = ttk.Entry(self)
         self.feet.grid(row=1, column=0, sticky="ew")
 
-        self.feetLabel = ttk.Label(self, text="Feet")
-        self.feetLabel.grid(row=2, column=0, sticky="ew")
+        self.feet_label = ttk.Label(self, text="Feet")
+        self.feet_label.grid(row=2, column=0, sticky="ew")
 
         self.inch = ttk.Entry(self)
         self.inch.grid(row=1, column=1, sticky="ew")
 
-        self.inchLabel = ttk.Label(self, text="Inches")
-        self.inchLabel.grid(row=2, column=1, sticky="ew")
+        self.inch_label = ttk.Label(self, text="Inches")
+        self.inch_label.grid(row=2, column=1, sticky="ew")
 
         self.frac = ttk.Entry(self)
         self.frac.grid(row=1, column=2, sticky="ew")
 
-        self.fracLabel = ttk.Label(self, text="Fraction")
-        self.fracLabel.grid(row=2, column=2, sticky="ew")
-
-
+        self.frac_label = ttk.Label(self, text="Fraction")
+        self.frac_label.grid(row=2, column=2, sticky="ew")
 
