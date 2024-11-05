ecacaad0ca30e99e8414d15515d095c1766913eb
nizn1770 <124064286+nizn1770@users.noreply.github.com>
2024-11-05 12:58:59 -0600

    Progress save

--- touchscreen.py
+++ touchscreen.py
@@ -9,7 +9,7 @@ def init_touchscreen():
     app.mainloop()
 
 
-class Application(tk.TK):
+class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Archimedes")
@@ -17,9 +17,9 @@ class Application(tk.TK):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
 
-        self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-        self.rowconfigure(2, weight=2)
+        self.rowconfigure(0, weight=3)
+        self.rowconfigure(1, weight=3)
+        self.rowconfigure(2, weight=1)
         
         hor = InputMeasure(self, "Horizontal")
         hor.grid(row=0, column=0, sticky="nsew")
@@ -27,32 +27,48 @@ class Application(tk.TK):
         vert = InputMeasure(self, "Vertical")
         vert.grid(row=1, column=0, sticky="nsew")
 
-        submit = ttk.Button(self, text="Send Cut", command=self.send_cut)
+        submit = ttk.Button(self, text="Send Cut")
+        submit.grid(row=2, column=0, sticky="nsew")
     
 
 
-class InputMeasure(ttk.frame):
+class InputMeasure(ttk.Frame):
     def __init__(self, parent, title_text):
         super().__init__(parent)
 
-        self.title = ttk.Label(self, text=title_text, justify="center")
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+
+        self.title = ttk.Label(self, text=title_text, justify='center')
         self.title.grid(row=0, column=0, columnspan=3, sticky="nsew")
 
         self.feet = ttk.Entry(self)
         self.feet.grid(row=1, column=0, sticky="ew")
 
-        self.feet_label = ttk.Label(self, text="Feet")
+        self.feet_label = ttk.Label(self, text="Feet", justify='center')
         self.feet_label.grid(row=2, column=0, sticky="ew")
 
         self.inch = ttk.Entry(self)
         self.inch.grid(row=1, column=1, sticky="ew")
 
-        self.inch_label = ttk.Label(self, text="Inches")
+        self.inch_label = ttk.Label(self, text="Inches", justify='center')
         self.inch_label.grid(row=2, column=1, sticky="ew")
 
         self.frac = ttk.Entry(self)
         self.frac.grid(row=1, column=2, sticky="ew")
 
-        self.frac_label = ttk.Label(self, text="Fraction")
+        self.frac_label = ttk.Label(self, text="Fraction", justify='center')
         self.frac_label.grid(row=2, column=2, sticky="ew")
+
+class KeyBoard(ttk.Frame):
+    def __init__(self, parent, num):
+        super().__init__(parent)
+
+        self.btn = ttk.Button(self, text=num)
+        self.btn.grid(row=0, column=0, sticky="nsew")
 
