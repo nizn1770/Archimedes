1ac96db3d5986b9a609d1b18ed2b51685b084f8d
nizn1770 <124064286+nizn1770@users.noreply.github.com>
2024-11-05 11:01:52 -0600

    InputMeasure build out

--- touchscreen.py
+++ touchscreen.py
@@ -14,6 +14,39 @@ class Application(tk.TK):
         super().__init__()
         self.title("Archimedes")
 
-class Input(ttk.frame, name):
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        
+        len = InputMeasure(self, "Length")
+        len.grid(row=0, column=0, sticky="nsew")
+
+        wid = InputMeasure(self, "Width")
+        wid.grid(row=0, column=0, sticky="nsew")
+
+class InputMeasure(ttk.frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.title = ttk.Label(self)
+
+
+        self.feet = ttk.Entry(self)
+        self.feet.grid(row=1, column=0, sticky="ew")
+
+        self.feetLabel = ttk.Label(self, text="Feet")
+        self.feetLabel.grid(row=2, column=0, sticky="ew")
+
+        self.inch = ttk.Entry(self)
+        self.inch.grid(row=1, column=1, sticky="ew")
+
+        self.inchLabel = ttk.Label(self, text="Inches")
+        self.inchLabel.grid(row=2, column=1, sticky="ew")
+
+        self.frac = ttk.Entry(self)
+        self.frac.grid(row=1, column=2, sticky="ew")
+
+        self.fracLabel = ttk.Label(self, text="Fraction")
+        self.fracLabel.grid(row=2, column=2, sticky="ew")
+
 
 
