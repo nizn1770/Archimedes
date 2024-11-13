34e488724040cc7597ead5bd0faf34d64c7c84c6
nizn1770 <124064286+nizn1770@users.noreply.github.com>
2024-11-13 16:06:30 -0600

    make progress buttons bigger

--- touchscreen.py
+++ touchscreen.py
@@ -73,17 +73,24 @@ class Application(tk.Tk):
         self.progress_window.attributes("-fullscreen", True)
         self.progress_window.title("Cutting")
 
-        progress_label = ttk.Label(self.progress_window, text="Cutting Board", font="Arial 16")
-        progress_label.pack(pady=10)
+        self.progress_window.columnconfigure(0, weight=1)
 
-        cut_length = ttk.Label(self.progress_window, text=message, font="Arial 16")
-        cut_length.pack(pady=10)
+        self.progress_window.rowconfigure(0, weight=1)
+        self.progress_window.rowconfigure(1, weight=1)
+        self.progress_window.rowconfigure(2, weight=1)
+        self.progress_window.rowconfigure(3, weight=3)
+
+        progress_label = ttk.Label(self.progress_window, text="Cutting Board", font="Arial 32", anchor="center")
+        progress_label.grid(row=0, column=0, sticky="nsew")
+
+        cut_length = ttk.Label(self.progress_window, text=message, font="Arial 24", anchor="center")
+        cut_length.grid(row=1, column=0, sticky="nsew")
 
         self.progress_bar = ttk.Progressbar(self.progress_window, maximum=100)
-        self.progress_bar.pack(pady=10, padx=10, fill='x')
+        self.progress_bar.grid(row=2, column=0, sticky="nsew")
 
         cancel_button = ttk.Button(self.progress_window, text="Cancel Cut", command=lambda: self.cancel_process())
-        cancel_button.pack(pady=10)
+        cancel_button.grid(row=3, column=0, sticky="nsew")
 
         threading.Thread(target=self.cut).start()
         self.logger.info("Cut Starting")
