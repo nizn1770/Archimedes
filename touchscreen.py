98e7b1577497de32d6c4788622fe24c23d91682b
nizn1770 <124064286+nizn1770@users.noreply.github.com>
2024-11-13 15:48:34 -0600

    make buttons bigger

--- touchscreen.py
+++ touchscreen.py
@@ -82,17 +82,24 @@ class Application(tk.Tk):
         self.confirmation_window.attributes("-fullscreen", True)
         self.confirmation_window.title("Confirm")
 
-        question_label = ttk.Label(self.confirmation_window, text="Is this the correct cut?", font="Arial 16")
-        question_label.pack(pady=10)
+        self.confirmation_window.columnconfigure(0, weight=1)
+        self.confirmation_window.columnconfigure(1, weight=1)
+        self.confirmation_window.rowconfigure(0, weight=1)
+        self.confirmation_window.rowconfigure(1, weight=1)
+        self.confirmation_window.rowconfigure(2, weight=1)
+        self.confirmation_window.rowconfigure(3, weight=1)
 
-        cut_length = ttk.Label(self.confirmation_window, text=message, font="Arial 16")
-        cut_length.pack(pady=10)
+        question_label = ttk.Label(self.confirmation_window, text="Is this the correct cut?", font="Arial 32", anchor='center')
+        question_label.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+        cut_length = ttk.Label(self.confirmation_window, text=message, font="Arial 24", anchor='center')
+        cut_length.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
         confirmation_button = ttk.Button(self.confirmation_window, text="Confirm", command=lambda: self.confirmation_result(True))
-        confirmation_button.pack(pady=10)
+        confirmation_button.grid(row=2, column=0, sticky="nsew")
 
         cancelation_button = ttk.Button(self.confirmation_window, text="Cancel", command=lambda: self.confirmation_result(False))
-        cancelation_button.pack(pady=10)
+        cancelation_button.grid(row=2, column=1, sticky="nsew")
 
 
     def confirmation_result(self, response):
