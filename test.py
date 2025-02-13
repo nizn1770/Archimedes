484189b38feb18e5bca285dc9799deec4e00c8e6
nizn1770 <124064286+nizn1770@users.noreply.github.com>
2025-02-13 13:09:30 -0600

    update test to go back to original working

--- test.py
+++ test.py
@@ -1,3 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+
+# Define GPIO pins
+MOTOR_PIN = 36  # Physical pin 11
+ALWAYS_HIGH_PIN = 27  # Use another available GPIO
+PWM_PIN = 22  # PWM pin
+PWM_FREQUENCY = 1000  # Frequency in Hz
+
+# GPIO setup
+GPIO.setmode(GPIO.BOARD)  # Use BOARD numbering
+GPIO.setwarnings(False)
+GPIO.setup(MOTOR_PIN, GPIO.OUT)
+GPIO.setup(ALWAYS_HIGH_PIN, GPIO.OUT)
+GPIO.setup(PWM_PIN, GPIO.OUT)
+GPIO.output(MOTOR_PIN, GPIO.HIGH)
+GPIO.output(ALWAYS_HIGH_PIN, GPIO.HIGH)  # Set this pin to always HIGH
+
+# Setup PWM with 50% duty cycle
+pwm = GPIO.PWM(PWM_PIN, PWM_FREQUENCY)
+pwm.start(50)
+
+# Motor state variable
+motor_on = False
+
+try:
+    while True:
+        input("Press Enter to toggle motor...")  # Wait for Enter key press
+        motor_on = not motor_on  # Toggle motor state
+        GPIO.output(MOTOR_PIN, GPIO.HIGH if motor_on else GPIO.LOW)
+        print("Motor ON" if motor_on else "Motor OFF")
+
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    pwm.stop()
+    GPIO.cleanup()  # Cleanup GPIO on exit
+
+
 # import RPi.GPIO as GPIO
 # import time
 # import keyboard  # Using keyboard module to detect key presses
@@ -45,70 +83,70 @@
 #     GPIO.cleanup()  # Cleanup GPIO on exit
 
 
-import RPi.GPIO as GPIO
-import time
-import keyboard  # Using keyboard module to detect key presses
+# import RPi.GPIO as GPIO
+# import time
+# import keyboard  # Using keyboard module to detect key presses
 
-# Define GPIO pins
-ENABLE_PIN = 36  # Enable pin for motor driver
-DIR_PIN = 27  # Direction pin
-PUL_PIN = 22  # Pulse pin for step signal
+# # Define GPIO pins
+# ENABLE_PIN = 36  # Enable pin for motor driver
+# DIR_PIN = 27  # Direction pin
+# PUL_PIN = 22  # Pulse pin for step signal
 
-# Step settings
-STEP_DELAY = 0.001  # Delay between step pulses (affects speed)
-STEPS_PER_MOVE = 200  # Number of steps per movement command
+# # Step settings
+# STEP_DELAY = 0.001  # Delay between step pulses (affects speed)
+# STEPS_PER_MOVE = 200  # Number of steps per movement command
 
-# GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(ENABLE_PIN, GPIO.OUT)
-GPIO.setup(DIR_PIN, GPIO.OUT)
-GPIO.setup(PUL_PIN, GPIO.OUT)
+# # GPIO setup
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+# GPIO.setup(ENABLE_PIN, GPIO.OUT)
+# GPIO.setup(DIR_PIN, GPIO.OUT)
+# GPIO.setup(PUL_PIN, GPIO.OUT)
 
-# Default states
-GPIO.output(DIR_PIN, GPIO.HIGH)  # Set default direction
-GPIO.output(ENABLE_PIN, GPIO.HIGH)  # Enable starts HIGH (disabled)
+# # Default states
+# GPIO.output(DIR_PIN, GPIO.HIGH)  # Set default direction
+# GPIO.output(ENABLE_PIN, GPIO.HIGH)  # Enable starts HIGH (disabled)
 
-enable_state = True  # Motor starts disabled
-direction_state = True  # Default direction HIGH
+# enable_state = True  # Motor starts disabled
+# direction_state = True  # Default direction HIGH
 
-def move_motor(steps):
-    """Moves the stepper motor a set number of steps."""
-    if not enable_state:  # Only move if enabled
-        for _ in range(steps):
-            GPIO.output(PUL_PIN, GPIO.HIGH)
-            time.sleep(STEP_DELAY)
-            GPIO.output(PUL_PIN, GPIO.LOW)
-            time.sleep(STEP_DELAY)
+# def move_motor(steps):
+#     """Moves the stepper motor a set number of steps."""
+#     if not enable_state:  # Only move if enabled
+#         for _ in range(steps):
+#             GPIO.output(PUL_PIN, GPIO.HIGH)
+#             time.sleep(STEP_DELAY)
+#             GPIO.output(PUL_PIN, GPIO.LOW)
+#             time.sleep(STEP_DELAY)
 
-try:
-    print("Controls: ENTER = Toggle Enable, UP = Toggle Direction")
+# try:
+#     print("Controls: ENTER = Toggle Enable, UP = Toggle Direction")
     
-    while True:
-        input("Press Enter to toggle motor...")  # Wait for Enter key press
-        enable_state = not enable_state
-        GPIO.output(ENABLE_PIN, GPIO.LOW if enable_state else GPIO.HIGH)
-        move_motor(STEPS_PER_MOVE)
+#     while True:
+#         input("Press Enter to toggle motor...")  # Wait for Enter key press
+#         enable_state = not enable_state
+#         GPIO.output(ENABLE_PIN, GPIO.LOW if enable_state else GPIO.HIGH)
+#         move_motor(STEPS_PER_MOVE)
 
 
-        # key = keyboard.read_event()
+#         # key = keyboard.read_event()
 
-        # if key.event_type == keyboard.KEY_DOWN:
-        #     if key.name == "enter":  # Toggle enable
-        #         enable_state = not enable_state
-        #         GPIO.output(ENABLE_PIN, GPIO.LOW if enable_state else GPIO.HIGH)
-        #         move_motor(STEPS_PER_MOVE)
+#         # if key.event_type == keyboard.KEY_DOWN:
+#         #     if key.name == "enter":  # Toggle enable
+#         #         enable_state = not enable_state
+#         #         GPIO.output(ENABLE_PIN, GPIO.LOW if enable_state else GPIO.HIGH)
+#         #         move_motor(STEPS_PER_MOVE)
 
-        #         print("Enable toggled:", "OFF" if enable_state else "ON")
+#         #         print("Enable toggled:", "OFF" if enable_state else "ON")
 
-        #     elif key.name == "up" and enable_state:  # Toggle direction
-        #         direction_state = not direction_state
-        #         GPIO.output(DIR_PIN, GPIO.HIGH if direction_state else GPIO.LOW)
-        #         print("Direction toggled:", "HIGH" if direction_state else "LOW")
+#         #     elif key.name == "up" and enable_state:  # Toggle direction
+#         #         direction_state = not direction_state
+#         #         GPIO.output(DIR_PIN, GPIO.HIGH if direction_state else GPIO.LOW)
+#         #         print("Direction toggled:", "HIGH" if direction_state else "LOW")
 
             
 
-except KeyboardInterrupt:
-    print("\nExiting program.")
-    GPIO.output(ENABLE_PIN, GPIO.HIGH)  # Ensure motor is disabled on exit
-    GPIO.cleanup()  # Cleanup GPIO on exit
+# except KeyboardInterrupt:
+#     print("\nExiting program.")
+#     GPIO.output(ENABLE_PIN, GPIO.HIGH)  # Ensure motor is disabled on exit
+#     GPIO.cleanup()  # Cleanup GPIO on exit
