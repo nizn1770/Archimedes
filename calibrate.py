c41d30c9ed16e4ea541429b953b2bd35cf7b7d78
nizn1770 <124064286+nizn1770@users.noreply.github.com>
2025-02-26 16:03:31 -0600

    make calibration.py, add pins to config

---
+++ calibrate.py
@@ -0,0 +1,66 @@
+import RPi.GPIO as GPIO
+import time
+import config
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+GPIO.setup(config.X_DIR_PIN, GPIO.OUT)
+GPIO.setup(config.X_PWM_PIN, GPIO.OUT)
+GPIO.setup(config.Y_DIR_PIN, GPIO.OUT)
+GPIO.setup(config.Y_PWM_PIN, GPIO.OUT)
+GPIO.setup(config.Z_DIR_PIN, GPIO.OUT)
+GPIO.setup(config.Z_PWM_PIN, GPIO.OUT)
+
+def rotate_motor(pin, distance, steps, frequency):
+
+    total_steps = int(distance * steps)
+    SLEEP_TIME = 1 / frequency
+
+    for _ in range(total_steps):
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(SLEEP_TIME) 
+        GPIO.output(pin, GPIO.LOW)
+        time.sleep(SLEEP_TIME)
+
+try:
+    while True:
+        motor = input("Enter motor (x/y/z): ")
+        distance = float(input("Enter distance (in inches): "))
+        
+        if motor == "x":
+            direction = input("Enter direction (l/r): ")
+            if direction == "l":
+                GPIO.output(config.X_DIR_PIN, GPIO.LOW)
+            else:
+                GPIO.output(config.X_DIR_PIN, GPIO.HIGH)
+
+            rotate_motor(config.X_PWM_PIN, distance, config.X_STEPS_PER_REV, config.X_PWM_FREQUENCY)
+
+            
+        if motor == "y":
+            direction = input("Enter direction (u/d): ")
+            if direction == "u":
+                GPIO.output(config.Y_DIR_PIN, GPIO.LOW)
+            else:
+                GPIO.output(config.Y_DIR_PIN, GPIO.HIGH)
+
+            rotate_motor(config.Y_PWM_PIN, distance, config.Y_STEPS_PER_REV, config.Y_PWM_FREQUENCY)
+
+        if motor == "z":
+            direction = input("Enter direction (u/d): ")
+            if direction == "u":
+                GPIO.output(config.Z_DIR_PIN, GPIO.LOW)
+            else:
+                GPIO.output(config.Z_DIR_PIN, GPIO.HIGH)
+
+            rotate_motor(config.Z_PWM_PIN, distance, config.Z_STEPS_PER_REV, config.Z_PWM_FREQUENCY)
+        
+        time.sleep(1)
+
+
+
+except KeyboardInterrupt:
+    print("Exiting")
+    GPIO.cleanup()
+
--- config.py
+++ config.py
@@ -15,11 +15,25 @@ MAX_VERTICAL = 48
 MIN_HORIZONTAL = 12
 MIN_VERTICAL = 12
 
+#Machine pin set ups
+X_DIR_PIN = 11
+X_PWM_PIN = 13
 
-STEPS_PER_INCH = 200
+X_PWM_FREQUENCY = 2000
+X_STEPS_PER_REV = 1000
 
-SERIAL_PORT = ""
-BAUD_RATE = 0
+Y_DIR_PIN = 15
+Y_PWM_PIN = 16
+
+Y_PWM_FREQUENCY = 2000
+Y_STEPS_PER_REV = 1000
+
+Z_DIR_PIN = 18
+Z_PWM_PIN = 22
+
+Z_PWM_FREQUENCY = 2000
+Z_STEPS_PER_REV = 1000
+
 
 DELETE_IMAGE = "https://raw.githubusercontent.com/nizn1770/Archimedes/main/images/delete.png"
 NEXT_IMAGE = "https://raw.githubusercontent.com/nizn1770/Archimedes/main/images/checkmark.png"
